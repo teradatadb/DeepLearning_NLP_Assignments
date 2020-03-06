6ae9853a9a0b305da55bfceeb2f4a8d0a7a676b3
teradatadb <110453239+teradatadb@users.noreply.github.com>
2020-03-06 07:23:05 +0000

    drm/i915: Fall back to bit banging mode for DVO transmitter detection

--- assignment1/q1_softmax.py
+++ assignment1/q1_softmax.py
@@ -3,3 +3,7 @@ import numpy as np
 
 
 def softmax(x):
+    """Compute the softmax function for each row of the input x.
+
+    It is crucial that this function is optimized for speed because
+    it will be used frequently in later code. You might find numpy
