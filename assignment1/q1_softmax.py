0ddec0090ff7bf3337ce9945dc703f9c94a4e212
teradatadb <110453239+teradatadb@users.noreply.github.com>
2020-03-06 22:19:31 +0000

    Added .gitconfig

--- assignment1/q1_softmax.py
+++ assignment1/q1_softmax.py
@@ -7,3 +7,10 @@ def softmax(x):
 
     It is crucial that this function is optimized for speed because
     it will be used frequently in later code. You might find numpy
+    functions np.exp, np.sum, np.reshape, np.max, and numpy
+    broadcasting useful for this task.
+
+    Numpy broadcasting documentation:
+    http://docs.scipy.org/doc/numpy/user/basics.broadcasting.html
+
+    You should also make sure that your code works for a single
