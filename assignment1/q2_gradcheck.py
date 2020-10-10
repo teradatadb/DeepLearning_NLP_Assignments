c0114058419718efe9139c30b5a811130c1c6d0c
teradatadb <110453239+teradatadb@users.noreply.github.com>
2020-10-10 06:48:12 +0000

    Units

--- assignment1/q2_gradcheck.py
+++ assignment1/q2_gradcheck.py
@@ -16,4 +16,4 @@ def gradcheck_naive(f, x):
 
     rndstate = random.getstate()
     random.setstate(rndstate)
-    fx, grad = f(x) # Evaluate function val
+    fx, grad = f(x) # Evaluate function value at 
