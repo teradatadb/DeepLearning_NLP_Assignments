9aa1a9ff36e3a9d5648d1618a8de56b93519be19
teradatadb <110453239+teradatadb@users.noreply.github.com>
2020-12-27 09:18:32 +0000

    3.6.3

--- assignment1/q2_gradcheck.py
+++ assignment1/q2_gradcheck.py
@@ -21,4 +21,4 @@ def gradcheck_naive(f, x):
 
     # Iterate over all indexes in x
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
-    while not it.fini
+    while not it.finished:
