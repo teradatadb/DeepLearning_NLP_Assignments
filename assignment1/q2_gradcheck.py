235b0b954251121862511096adc7275ca12c364f
teradatadb <110453239+teradatadb@users.noreply.github.com>
2022-03-29 07:53:31 +0000

    More debug

--- assignment1/q2_gradcheck.py
+++ assignment1/q2_gradcheck.py
@@ -62,4 +62,4 @@ def sanity_check():
     quad = lambda x: (np.sum(x ** 2), x * 2)
 
     print("Running sanity checks...")
-    gradcheck_naive(quad, np.array(123.456))      # scala
+    gradcheck_naive(quad, np.array(123.456))      # scalar test
