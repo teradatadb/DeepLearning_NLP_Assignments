7797a4677fdcfa0e75db9539a57bbb78a86db6ac
teradatadb <110453239+teradatadb@users.noreply.github.com>
2022-07-23 10:06:13 +0000

    add godoc badge

--- assignment1/q2_gradcheck.py
+++ assignment1/q2_gradcheck.py
@@ -75,4 +75,5 @@ def your_sanity_checks():
     This function will not be called by the autograder, nor will
     your additional tests be graded.
     """
-    print("Running your sanity checks.
+    print("Running your sanity checks...")
+ 
