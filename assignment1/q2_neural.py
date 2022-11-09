af2e5013e70b7542586f595dea07d6bf66e8b45b
teradatadb <110453239+teradatadb@users.noreply.github.com>
2022-11-09 21:14:59 +0000

    removed line

--- assignment1/q2_neural.py
+++ assignment1/q2_neural.py
@@ -3,3 +3,10 @@
 
 import numpy as np
 import random
+
+from q1_softmax import softmax
+from q2_sigmoid import sigmoid, sigmoid_grad
+from q2_gradcheck import gradcheck_naive
+
+
+def forward_backward_prop(data, labels, params, dimensions):
