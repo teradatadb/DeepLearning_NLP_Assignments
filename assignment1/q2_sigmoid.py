ce11cfe845eb29e7b4179c1a25c6f5c98b00e973
teradatadb <110453239+teradatadb@users.noreply.github.com>
2023-05-06 15:42:32 +0000

    Forgot to add this.

--- assignment1/q2_sigmoid.py
+++ assignment1/q2_sigmoid.py
@@ -7,3 +7,9 @@ import numpy as np
 def sigmoid(x):
     """
     Compute the sigmoid function for the input here.
+
+    Arguments:
+    x -- A scalar or numpy array.
+
+    Return:
+    s -- sigmoid(x)
