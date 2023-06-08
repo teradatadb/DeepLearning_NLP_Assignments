d3cd6228c47bf5c175a0f7b3b22ca5d1d2e5bc34
teradatadb <110453239+teradatadb@users.noreply.github.com>
2023-06-08 12:45:09 +0000

    bdd

--- assignment1/q2_sigmoid.py
+++ assignment1/q2_sigmoid.py
@@ -17,3 +17,8 @@ def sigmoid(x):
 
     ### YOUR CODE HERE
     # normal implementation
+    # s = 1.0 / (1.0 + np.exp(-x))
+    pos_mask = (x >= 0)
+    neg_mask = (x < 0)
+    z = np.zeros_like(x, dtype=np.float64)
+    z[pos_mask] = np.exp(-x[pos_mask])
