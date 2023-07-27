d398588817fe4131b6db2925e71c6f941352aeeb
teradatadb <110453239+teradatadb@users.noreply.github.com>
2023-07-27 10:08:29 +0000

    pfft

--- assignment1/q2_sigmoid.py
+++ assignment1/q2_sigmoid.py
@@ -22,3 +22,8 @@ def sigmoid(x):
     neg_mask = (x < 0)
     z = np.zeros_like(x, dtype=np.float64)
     z[pos_mask] = np.exp(-x[pos_mask])
+    z[neg_mask] = np.exp(x[neg_mask])
+    t = np.ones_like(x, dtype=np.float64)
+    t[neg_mask] = z[neg_mask]
+    s = t / (1 + z)
+    ### END YOUR CODE
